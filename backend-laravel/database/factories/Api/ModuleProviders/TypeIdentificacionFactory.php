c9ea38c4ddaa4e0b67f2f3d0a5c41366e9ac20b9
Jhon Acosta <jhon.erick.acosta@gmail.com>
2021-07-24 22:11:35 -0500

    Estructura de carpetas

---
+++ backend-laravel/database/factories/Api/ModuleProviders/TypeIdentificacionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\Api\ModuleProviders;
+
+use App\Models\Api\ModuleProviders\TypeIdentificacion;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TypeIdentificacionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TypeIdentificacion::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
