2f7313e4cb6f0465cb0a36dbf3cec3dfe021e0e6
Jhon Acosta <jhon.erick.acosta@gmail.com>
2021-08-22 13:49:45 -0500

    Mejora en el modulo

--- frontend-angular/src/app/module-providers/remember-password/remember-password.component.ts
+++ frontend-angular/src/app/module-providers/remember-password/remember-password.component.ts
@@ -1,3 +1,4 @@
+import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit } from '@angular/core';
 import { RememberPassowrdI } from '../interfaces/Interfaces';
 import { RememberPasswordService } from '../services/remember-password.service';
@@ -16,33 +17,33 @@ export class RememberPasswordComponent implements OnInit {
   public success = ''
   public error = ''
 
-  constructor(private _email: RememberPasswordService) { }
+  constructor(
+    private _email: RememberPasswordService,
+    private toastr: ToastrService,
+  ) { }
 
-  timeMessage (msg: string, action?: string) {
-    if ( action === 'error') {
-      this.error = msg
+  timeMessage(msg: string, action?: string) {
+    if (action === 'error') {
+      return this.toastr.error(`${msg}`, 'Error');
     } else {
       this.success = msg
+      return this.toastr.success(`${msg}`, 'Correo enviado');
     }
-    setTimeout(() => {
-      this.error = ''
-      this.success = ''
-    }, 3500);
   }
 
-  async rememberPassword () {
-    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.exec(this.data.email) || 
-    this.data.email == ''
-    ) { return this.timeMessage('Se requiere de un correo electrónico', 'error')}
+  async rememberPassword() {
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.exec(this.data.email) ||
+      this.data.email == ''
+    ) { return this.timeMessage('Se requiere de un correo electrónico', 'error') }
     try {
       await this._email.rememberPassword(this.data).subscribe(res => {
         if (res.data.message === 'Email not found') {
           return this.timeMessage('Correo electrónico no registrado', 'error')
         }
         return this.timeMessage(
-          'Correo enviado, revise su banjeda de entrada.',
+          'Revise su banjeda de entrada.',
           'success'
-          )
+        )
       })
     } catch (error) { console.log(error) }
   }
@@ -50,5 +51,5 @@ export class RememberPasswordComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  
+
 }
